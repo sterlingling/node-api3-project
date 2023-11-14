cb1a2d84ada28a3bcaf8b770331cfe1ee796faf5
Sterling Sanders <sterlingsanders723@gmail.com>
2023-11-14 14:55:43 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it
+const server = require('./api/server.js');
+
+const port = 2000;
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
